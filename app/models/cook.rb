1879cd69b7e6bff73fbfac790df5e35861fbead8
yamassx <58455442+yamassx@users.noreply.github.com>
2020-02-08 14:31:12 +0900

    バリデーション追記

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -3,5 +3,5 @@ class Cook < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, :last_name, :nickname
+  validates :first_name, :last_name, :nickname, presence: true
 end
