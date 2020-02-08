97ad84750fc78f3a14e7d1b076b2a4717b78b210
yamassx <58455442+yamassx@users.noreply.github.com>
2020-02-08 12:21:15 +0900

    ルーティングの整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :reataurants
-  devise_for :cooks
+  devise_for :reataurants, controllers: {
+    sessions:      'reataurants/sessions',
+    passwords:     'reataurants/passwords',
+    registrations: 'reataurants/registrations'
+  }
+  devise_for :cooks, controllers: {
+    sessions:      'cooks/sessions',
+    passwords:     'cooks/passwords',
+    registrations: 'cooks/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
