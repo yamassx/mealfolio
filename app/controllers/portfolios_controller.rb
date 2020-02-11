96768400da7ca866b810147af2ee01583662dc91
yamassx <58455442+yamassx@users.noreply.github.com>
2020-02-11 19:16:30 +0900

    文字投稿可能

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,19 +7,19 @@ class PortfoliosController < ApplicationController
     # @portfolio.pfimage.new
   end
   def create
-    @portfolio = Portfolio.create(portfolio_params)
-    # if @portfolio.save
-    #   redirect_to cook_portfolios_path(@group), notice: 'メッセージが送信されました'
-    # else
-    #   @portfolios = @cook.portfolios.includes(:cook)
-    #   flash.now[:alert] = 'メッセージを入力してください。'
-    #   render :index
-    # end
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to root_path, notice: '投稿されました'
+    else
+      # @portfolios = @cook.portfolios.includes(:cook)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
 
   private
 
     def portfolio_params
-      params.require(:portfolio).permit(:title, :technic, :detale, images_attributes: [:image, :_destroy, :id]).merge(cook_id: current_cook.id)
+      params.require(:portfolio).permit(:title, :tecnic, :detale).merge(cook_id: current_cook.id)
     end
 end
