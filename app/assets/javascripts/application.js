088678f6198dab3dd2dcd978f8d055d454ff434f
yamassx <58455442+yamassx@users.noreply.github.com>
2020-02-19 03:21:23 +0900

    jsファイル関係を整理

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-$(function() {
-  $('.landing_page__contents__image_slicker').not('.slick-initialized').slick({
-    autoplay: true,
-    fade: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-    arrows: false,
-    autoplaySpeed: 2000,
-  });
-});
