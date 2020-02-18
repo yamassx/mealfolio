03ccc3983bccbffd07585eb75bc19fecf48b5d4d
yamassx <58455442+yamassx@users.noreply.github.com>
2020-02-18 16:38:02 +0900

    スライドショー実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,16 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
+$(function() {
+  $('.landing_page__contents__image_slicker').not('.slick-initialized').slick({
+    autoplay: true,
+    fade: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+  });
+});
