fa822d5128b61b614d0923508b0614ba9779e236
yamassx <58455442+yamassx@users.noreply.github.com>
2020-02-25 16:53:50 +0900

    ログイン、ログアウト後の処理（調理人のみ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "portfolios#lp"
+  root  "portfolios#lp"
   devise_for :restaurants, controllers: {
     sessions:      'restaurants/sessions',
     passwords:     'restaurants/passwords',
