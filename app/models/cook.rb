b77e38d551bdb18495207283a54d741ae9ae5f8c
yamassx <58455442+yamassx@users.noreply.github.com>
2020-02-26 18:30:04 +0900

    アップローダーの追加と設定

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -5,5 +5,6 @@ class Cook < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :nickname, presence: true
   has_many :portfolios
+  mount_uploader :face, :background, CookimageUploader
   
 end
