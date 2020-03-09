2a36fdcfffef8ce5de86666149092da5da52b58f
yamassx <58455442+yamassx@users.noreply.github.com>
2020-03-09 12:19:42 +0900

    seed記入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 cook = Cook.create!(
   nickname: "板前",
   email: "c@com",
-  password: "itamae3"
+  password: "itamae3",
+  first_name: "板前",
+  last_name: "太郎",
+  greeting: "和食調理人を経て、食品関係の仕事をしてきました。現在はRuby勉強中です。",
+  history: "XX年〜XX年、割烹　A　"
 )
